d2d09ac46e9314e974d94c92375002e359c6c41b
Pontus Pohl <pontus.pohl@gmail.com>
2016-05-18 16:47:23 +0200

    Fix

--- casper_lidar_scanner_driver/include/sockethandler.hpp
+++ casper_lidar_scanner_driver/include/sockethandler.hpp
@@ -43,7 +43,7 @@ class sockethandler{
         void handle_receive(const boost::system::error_code& error,
         std::size_t bytes_transferred);
     public:
-        sockethandler(void);
+        //sockethandler(void);
         sockethandler(lidarserver* server, int port);
         ~sockethandler();
         void start();
